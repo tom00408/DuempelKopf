a1e8e60d3b2fd651dc7f0b9631f02ed6c2f2ce69
tom00408 <tom.tiedtke@aol.com>
2025-02-18 16:40:39 +0100

    Initial Commit

---
+++ DuempelKopf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DuempelKopf
+//
+//  Created by Tom Tiedtke on 18.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DuempelKopf/DuempelKopfApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuempelKopfApp.swift
+//  DuempelKopf
+//
+//  Created by Tom Tiedtke on 18.02.25.
+//
+
+import SwiftUI
+
+@main
+struct DuempelKopfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
