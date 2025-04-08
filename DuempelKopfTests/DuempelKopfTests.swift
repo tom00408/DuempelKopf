c9bd4ea035a6fa2f53c37c7c793617644e03a4d2
tom00408 <tom.tiedtke@aol.com>
2025-04-08 21:43:23 +0200

    Tests hinzugefügt

---
+++ DuempelKopfTests/DuempelKopfTests.swift
@@ -0,0 +1,20 @@
+//
+//  DuempelKopfTests.swift
+//  DuempelKopfTests
+//
+//  Created by Tom Tiedtke on 07.04.25.
+//
+@testable import DuempelKopf
+import Testing
+
+struct DuempelKopfTests {
+
+    @Test func testBocke() throws {
+            let myList = List(name: "BOCKTESTLISTE", players: ["Tom", "Anna"], info: "Test Info")
+            let viewModel = SingleListViewModel(list: myList)
+
+            #expect(viewModel.list.name == "BOCKTESTLISTE")
+            #expect(viewModel.list.name == "BOCKTESTLISTE")
+    }
+
+}
